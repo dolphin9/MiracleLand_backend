f7aeacd30a5623d1a3f720510dc4c7920aa509f1
dolphin9 <dolphin9@foxmail.com>
2025-12-25 04:49:45 +0800

    响应式导航栏（不成功）

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     // 导航栏交互效果
     const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarCollapse = document.querySelector('.collapse');
+    const navbarCollapse = document.querySelector('header u1');
 
     navbarToggler.addEventListener('click', function() {
         navbarCollapse.classList.toggle('show');
